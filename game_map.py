c67ac23fda620434191e3cdf22cc3f197e63036c
Sebas2545 <sjgonzalez00@gmail.com>
2024-08-15 12:10:51 -0700

    part 5 - placing enemies place enemies randomly in rooms and enabled collision and interaction with them

--- game_map.py
+++ game_map.py
@@ -1,16 +1,32 @@
+from __future__ import annotations
+
+from typing import Iterable, Optional, TYPE_CHECKING
+
+
 import numpy as np # type: ignore
 from tcod.console import Console
 
 import tile_types
 
+if TYPE_CHECKING:
+    from entity import Entity
+
 
 class GameMap:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int, entities: Iterable[Entity] = ()):
         self.width, self.height = width, height
+        self.entities = set(entities)
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
         self.visible = np.full((width, height), fill_value=False, order="F") # tiles player can see
         self.explored = np.full((width, height), fill_value=False, order="F") # tiles player has seen before
+
+    def get_blocking_entity_at_location(self, location_x: int, location_y: int) -> Optional[Entity]:
+        for entity in self.entities:
+            if entity.blocks_movement and entity.x == location_x and entity.y == location_y:
+                return entity
+            
+        return None
         
 
     def in_bounds(self, x: int, y: int) -> bool:
@@ -31,3 +47,8 @@ class GameMap:
             choicelist=[self.tiles["light"], self.tiles["dark"]], # if true in visible, tile is "light", else if explored is true, tile is "dark"
             default=tile_types.SHROUD # if neither is true, tile is shrouded
         )
+
+        for entity in self.entities:
+            # only print entities in FOV
+            if self.visible[entity.x, entity.y]:
+                console.print(x=entity.x, y=entity.y, string=entity.char, fg=entity.color)
