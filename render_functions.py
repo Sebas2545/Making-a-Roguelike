b693c3a8441857cf5b22d316723238ddb171bd5c
Sebas2545 <sjgonzalez00@gmail.com>
2024-08-30 00:30:01 -0700

    Part 11 - Delving into the Dungeon; added multiple floors to the dungeon and a leveling system

--- render_functions.py
+++ render_functions.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import Tuple, TYPE_CHECKING
 
 import color
 
@@ -37,6 +37,16 @@ def render_bar(
     console.print(
         x=1, y=45, string=f"HP: {current_value}/{maximum_value}", fg=color.bar_text
     )
+
+def render_dungeon_level(
+        console: console, dungeon_level: int, location: Tuple[int, int]
+) -> None:
+    """
+    render the level the player is currently on, at the given location
+    """
+    x, y = location
+
+    console.print(x=x, y=y, string=f"Dungeon Level: {dungeon_level}")
 
 def render_names_at_mouse_location(
         console: console, x: int, y: int, engine: Engine
